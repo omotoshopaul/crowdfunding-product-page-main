ce83fc1c79942b1e435a95eccc3ed2515529d5f4
CEHA <omotoshopaul688@gmail.comgit config --global user.email>
2025-12-15 23:38:49 +0100

    6th Comit

--- script.js
+++ script.js
@@ -50,9 +50,42 @@ const discoverButton = document.getElementById("discoverName");
     backThisProject.classList.remove("hidden");
   });
 
-  const radioBambooStandsRadio = document.getElementById("radioBambooStands");
-  const footerBamboo = document.getElementById("footerBamboo");
+  const radioBambooStandsRadio = document.getElementById("radioBambooStands"); 
+const footerBamboo = document.getElementById("footerBamboo");
+const radioPledgeWithNoReward = document.getElementById("radioPledgeWithNoReward"); 
+
+const radioBlackEditionStand = document.getElementById("radioBlackEditionStand"); 
+const footerBlackEdition = document.getElementById("footerBlackEdition");
+const pledgeWithNowRewardContinue = document.getElementById("pledgeWithNowRewardContinue");
+
+function closeAllFooters() {
+  footerBamboo.classList.add("hidden");
+  footerBlackEdition.classList.add("hidden");
+  pledgeWithNowRewardContinue.classList.add("hidden");
+}
 
 radioBambooStandsRadio.addEventListener("change", () => {
-    footerBamboo.classList.remove("hidden");
+  closeAllFooters();
+  footerBamboo.classList.remove("hidden");
+});
+
+radioBlackEditionStand.addEventListener("change", () => {
+  closeAllFooters();
+  footerBlackEdition.classList.remove("hidden");
+});
+ radioPledgeWithNoReward.addEventListener("change", () => { 
+   closeAllFooters();
+   pledgeWithNowRewardContinue.classList.remove("hidden");
+ });
+
+const pledgeWithNowRewardContinueButton = document.getElementById("pledgeWithNowRewardContinue");
+const thankYouPopUp = document.getElementById("thankYouPopUp");
+const closeThankYouPopUp = document.getElementById("closeThankYouPopUp"); 
+closeThankYouPopUp.addEventListener("click", () => {
+    thankYouPopUp.classList.add("hidden");
+  });
+  pledgeWithNowRewardContinueButton.addEventListener("click", (e) => {
+    e.stopPropagation();
+   backThisProject.classList.add("hidden");
+    thankYouPopUp.classList.remove("hidden");
   });
